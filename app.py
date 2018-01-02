1080af8bcb1c29d10985b516113c5519d1ab67c4
Tiffany Chen <tiffanysta.c@gmail.com>
2018-01-02 14:58:50 -0500

    added a route

--- app.py
+++ app.py
@@ -10,14 +10,16 @@ def root():
 
 @app.route('/board')
 def board():
-	
 
 @app.route('/slots')
 def slots():
 	return render_template('slots.html')
 
-
-
+@app.route('/oldGame')
+def oldGame():
+	game = request.form["game"]
+	passcode = request.form["passcode"]
+	return redirect(url_for("board"))
 
 if __name__ == '__main__':
 	app.debug = True
