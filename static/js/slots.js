eab5b7f139ecd53f12d2b312d676f5a16bf5459a
JonathanQuang <jquang@stuy.edu>
2018-01-08 13:21:03 -0500

    slots update

--- static/js/slots.js
+++ static/js/slots.js
@@ -1,11 +1,48 @@
+var interrupt = false;
+
+
 var animateVertical = function(){
-	var box1 = document.getElementById("box1");
-	box1.style.top="-100px";
+	var leftSlot = document.getElementById("leftWheel");
+	var verticalPos = 0;
+	var id = setInterval(moveDown,5);
+	console.log('interva');
+	function moveDown(){
+		if (verticalPos > 250) {
+			
+		}
+		if (interrupt == true){
+			clearInterval(id);
+		}
+		if (verticalPos == 300){
+			verticalPos=0;
+			leftSlot.style.top = verticalPos + 'px';
+		}
+		else {
+			//console.log('else ticked');
+			verticalPos++;
+			leftSlot.style.top = verticalPos + 'px';
+		}
+	}
 	console.log("shift");
 }
 
 
-var pleaseWork = function(){
-	console.log("js loaded");
+//var slotVel = 5
+//var id = setInterval(move
+
+
+
+document.addEventListener("keypress",lockWheel);
+
+var lockWheel = function(pressEvent){
+	console.log("keypress detected");
+	console.log(pressEvent);	
+	if (pressEvent.key == "i"){
+		console.log("i pressed");		
+		interrupt = true;
+		console.log(interrupt);	
+	}
+	
+	
 }
 
