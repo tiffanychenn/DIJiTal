09c0434d7350a529bc9f49a178bf496ff5a387f2
JonathanQuang <jquang@stuy.edu>
2018-01-11 01:58:24 -0500

    slots done for one player

--- static/js/slots.js
+++ static/js/slots.js
@@ -1,51 +1,104 @@
 //Declare globalVars
-var interrupt = false;
+var p1Toggle = false;
+var p1Left = false;
+var p1Middle = false;
+var p1Right = false;
+var p1Results = ["N/A","N/A","N/A"];
+
+var verticalPosRedToResult = function(slotArray){
+	//console.log(slotArray);
+	var arrayOfSimilarity = [0,0,0,0,0];
+	for (i=0;i<4;i++){
+		var topString = slotArray[i]["divRef"].style.top;
+		topString = topString.slice(0,topString.length-2);
+		arrayOfSimilarity[i]=parseFloat(topString)-200;
+	}
+	console.log(arrayOfSimilarity);
+
+	var colorIndex = -1;
+	for (i=0;i<4;i++){
+		if (arrayOfSimilarity[i] <=0 && arrayOfSimilarity[i] > -100){
+			colorIndex = i;
+		}
+	}
+	console.log(colorIndex + "colorIndex");	
+	if (colorIndex==0){
+		return "red";
+	}
+	if (colorIndex==1){
+		return "blue";
+	}	
+	if (colorIndex==2){
+		return "green";
+	}
+	if (colorIndex==3){
+		return "yellow";
+	}
+
+	return "black";
+}
 
 
-var animateVertical = function(wheelDivID,defaultVerticalDelta){
-	var leftSlot = document.getElementById(wheelDivID);
-	var verticalPos = defaultVerticalDelta;
-	var id = setInterval(moveDown,0.05);
+var animateVertical = function(wheelDivCol,speedDelta){
+	var slot0 = {"divRef": document.getElementById(wheelDivCol+"0"),"start":0};
+	var slot1 = {"divRef": document.getElementById(wheelDivCol+"1"),"start":100};
+	var slot2 = {"divRef": document.getElementById(wheelDivCol+"2"),"start":200};
+	var slot3 = {"divRef": document.getElementById(wheelDivCol+"3"),"start":300};
+	var slotArray = [slot0,slot1,slot2,slot3];
+	
+
+
+	var id = setInterval(moveDown,0.1);
 	
 	function moveDown(){
-		//console.log(verticalPos);
-		if (verticalPos < 100) {
-			var heightDelta = verticalPos;
-			leftSlot.style.height = heightDelta;
-			leftSlot.style.top = 0; 
+		for (i = 0; i < 4; i++){
+		
+			if (slotArray[i]["start"] < 100) {
+				var heightDelta = slotArray[i]["start"];
+				slotArray[i]["divRef"].style.height = heightDelta;
+				slotArray[i]["divRef"].style.top = 0; 
+			}
+			else if (slotArray[i]["start"] >= 100 && slotArray[i]["start"] <= 400){
+				slotArray[i]["divRef"].style.height = 100;
+				slotArray[i]["divRef"].style.top = slotArray[i]["start"]-100;
+			}
+			else if (slotArray[i]["start"] > 400) {
+				var heightDelta = slotArray[i]["start"] - 400; 
+				//console.log(heightDelta);
+				slotArray[i]["divRef"].style.height = 100 - heightDelta;
+				slotArray[i]["divRef"].style.top = 300 + heightDelta + 'px';			
+			}
+			if ((p1Middle == true && wheelDivCol=="middleWheel")||(p1Left == true && wheelDivCol=="leftWheel")||(p1Right == true && wheelDivCol=="rightWheel")){
+				clearInterval(id);
+				var floatPositionOfRed = slotArray[0]["start"];
+				if (wheelDivCol == "leftWheel"){
+					p1Results[0]=verticalPosRedToResult(slotArray);
+				}
+				if (wheelDivCol == "middleWheel"){
+					p1Results[1]=verticalPosRedToResult(slotArray);
+				}
+				if (wheelDivCol == "rightWheel"){
+					p1Results[2]=verticalPosRedToResult(slotArray);
+				}
+				document.getElementById("p1Results").innerText = p1Results[0] + " " + p1Results[1] + " " + p1Results[2];
+			}
+			if (slotArray[i]["start"] >= 500){
+				slotArray[i]["start"]=0;
+				slotArray[i]["divRef"].style.height = 100;
+				slotArray[i]["divRef"].style.top = slotArray[i]["start"] + 'px';
+			}
+			slotArray[i]["start"]+=speedDelta;
 		}
-		else if (verticalPos >= 100 && verticalPos <= 400){
-			leftSlot.style.height = 100;
-			leftSlot.style.top = verticalPos-100;
-		}
-		else if (verticalPos > 400) {
-			var heightDelta = verticalPos - 400; 
-			//console.log(heightDelta);
-			leftSlot.style.height = 100 - heightDelta;
-			leftSlot.style.top = 300 + heightDelta + 'px';			
-		}
-		if (interrupt == true){
-			clearInterval(id);
-		}
-		if (verticalPos == 500){
-			verticalPos=0;
-			leftSlot.style.height = 100;
-			leftSlot.style.top = verticalPos + 'px';
-		}
-		verticalPos++;
 	}
-	console.log("shift");
 }
 
 var animateLeft = function(){
-	animateVertical("leftWheel0",0);
-	animateVertical("leftWheel1",100);
-	animateVertical("leftWheel2",200);
-	animateVertical("leftWheel3",300);
-	animateVertical("middleWheel0",0);
-	animateVertical("middleWheel1",100);
-	animateVertical("middleWheel2",200);
-	animateVertical("middleWheel3",300);
+	var stringArray = ["leftWheel","middleWheel","rightWheel"];
+	for (i = 0; i < stringArray.length;i++){
+		var speedVar = Math.random()*8+2;
+		animateVertical(stringArray[i],speedVar);
+	}
+
 }
 
 
@@ -61,15 +114,21 @@ document.addEventListener("keypress",playerControls);
 var playerControls = function(pressEvent){
 	console.log("keypress detected");
 	console.log(pressEvent);	
-	if (pressEvent.key == "i"){
-		console.log("i pressed");		
-		interrupt = true;
-		console.log(interrupt);	
-	}
 	if (pressEvent.key == "k"){
-		console.log("k pressed");
+		console.log("k pressed");		
+		p1Middle = true;	
+	}
+	if (pressEvent.key == "j"){
+		p1Left = true;
+	}
+	if (pressEvent.key == "l"){
+		p1Right=true;
+	}
+	if (pressEvent.key == "i" && p1Toggle == false){
+		p1Toggle = true;
 		animateLeft();
 	}
+
 	
 	
 }
