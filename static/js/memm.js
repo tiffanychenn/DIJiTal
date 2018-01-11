dd8700e6ad47e6430be47cd0f759666cec81291f
Iris Tao <mtao@stuy.edu>
2018-01-11 13:25:18 -0500

    memm

---
+++ static/js/memm.js
@@ -0,0 +1,28 @@
+var flipped1;
+var flipped2;
+var timepassed;
+
+function setBoard(){
+    var card = document.getElementsByClassName('card');
+    var pics = ["url('images/100.jpg')", "url('images/101.jpg')", "url('images/102.jpg')", "url('images/103.jpg')", "url('images/104.jpg')", "url('images/105.jpg')", "url('images/106.jpg')", "url('images/107.jpg')", "url('images/100.jpg')", "url('images/101.jpg')", "url('images/102.jpg')", "url('images/103.jpg')", "url('images/104.jpg')", "url('images/105.jpg')", "url('images/106.jpg')", "url('images/107.jpg')"];
+
+    timepassed = 59;
+    flipped1 = [];
+    flipped2 = [];
+
+}
+
+function flipCard(){
+}
+
+function turnBack(){
+}
+
+function shuffle(){
+}
+
+function startTiming(){
+}
+
+function gameOverDisplay(){
+}
