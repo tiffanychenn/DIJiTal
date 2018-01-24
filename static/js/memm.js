48da0f7bbbccf95b04f576737568b3987763dd17
Iris Tao <mtao@stuy.edu>
2018-01-23 22:42:52 -0500

    mm

--- static/js/memm.js
+++ static/js/memm.js
@@ -1,34 +1,191 @@
-var flipped1;
-var flipped2;
-var timepassed;
+(function() {
 
-function setBoard(){
-    var card = document.getElementsByClassName('card');
-    var pics = ["url('../../pics/1.png')", "url('../../pics/2.png')", "url('../../pics/3.png')", "url('../../pics/4.png')", "url('../../pics/5.png')", "url('../../pics/6.png')"];
+	var flippedCards = []; //flipped cards collection for player
+	var flippedCards1 = []; //flipped cards collection for player1
+	var score = 0;
+	var score1 = 0;
+	var endGame = document.getElementsByClassName('game-over')[0];
+	var scoreD = document.getElementsByClassName('scoreDisplay')[0];
+	var scoreD1 = document.getElementsByClassName('scoreDisplay1')[0];
 
-    timepassed = 59;
-    flipped1 = [];
-    flipped2 = [];
+	function setBoard(){	
+		endGame.style.display = 'none';
+		var card = document.getElementsByClassName('card');
+		var card1 = document.getElementsByClassName('card1');
 
-    shuffle(pics);
+		var pics = ["url('../pics/1.png')", "url('../pics/2.png')", "url('../pics/3.png')","url('../pics/1.png')", "url('../pics/2.png')", "url('../pics/3.png')" ];
 
-    for (var i = 0; i < card.length; i++){
-    	if(card[i].)
-    }
+		var pics1 = ["url('../pics/4.png')", "url('../pics/5.png')", "url('../pics/6.png')","url('../pics/4.png')", "url('../pics/5.png')", "url('../pics/6.png')"];
 
-}
+		shuffle(pics); 
+		shuffle(pics1); 
+		
+	    for (var i = 0; i < card.length; i++){
+	    	if(card[i].classList.contains('flipped')){
+	    		card[i].classList.toggle('flipped');
+	    	}
+	    	card[i].querySelector('.back').style.backgroundImage = pics[i];
+	    	card[i].addEventListener('click', flipCard);  	
+	    }
 
-function flipCard(){
-}
 
-function turnBack(){
-}
+	    for (var i = 0; i < card1.length; i++){
+	    	if(card1[i].classList.contains('flipped')){
+	    		card1[i].classList.toggle('flipped');
+	    	}
+	    	card1[i].querySelector('.back').style.backgroundImage = pics1[i];
+	    	card1[i].addEventListener('click', flipCard1);  	
+	    }
 
-function shuffle(){
-}
+	}
 
-function startTiming(){
-}
+	//------DONE------------------------------------------------------
+	function flipCard(){ //run when the card (this) is clicked
+		//first, check if this card has already been flipped
 
-function gameOverDisplay(){
-}
+		var flippedTest = this.classList.contains('flipped');
+
+		if (!flippedTest && flippedCards.length < 2){
+			this.classList.toggle('flipped');
+			flippedCards.push(this);
+
+			if (flippedCards.length == 2){ 
+			//if two cards are flipped, compare them
+				matchTest();
+				console.log('nowitschekcingmath');
+
+			}
+		}	
+	}
+
+	function flipCard1(){ //run when the card (this) is clicked
+		//first, check if this card has already been flipped
+
+		var flippedTest = this.classList.contains('flipped');
+
+		if (!flippedTest && flippedCards1.length < 2){
+			this.classList.toggle('flipped');
+			flippedCards1.push(this);
+
+			if (flippedCards1.length == 2){ 
+			//if two cards are flipped, compare them
+				matchTest1();
+				console.log('nowitschekcingmath');
+
+			}
+		}	
+	}
+
+	//------DONE------------------------------------------------------
+	function turnBack(){
+		for (var i = 0; i < 2; i++){
+			flippedCards[i].classList.toggle('flipped');
+		}
+		flippedCards = [];
+		// clear flipped list
+
+	}
+
+	function turnBack1(){
+		for (var i = 0; i < 2; i++){
+			flippedCards1[i].classList.toggle('flipped');
+		}
+		flippedCards1 = [];
+		// clear flipped list
+
+	}
+
+	//------DONE------------------------------------------------------
+	function matchTest(){
+		//if the first card flipped refers to the same object (the back image) as the second one does
+		if (flippedCards[0].querySelector('.back').style.backgroundImage === flippedCards[1].querySelector('.back').style.backgroundImage){
+
+			//clear the flipped list
+			flippedCards = [];
+
+			score ++;
+			//console.log(score);
+			
+			scoreD.querySelector('h2').innerText = score;
+			
+			if (score == 3){
+				gameOverDisplay();
+				
+			}
+		}
+
+		//if they fail to match
+		else{
+			//wait for 1 sec, flipBack the card.
+			setTimeout(turnBack, 1000);
+		}
+	}
+
+	function matchTest1(){
+		//if the first card flipped refers to the same object (the back image) as the second one does
+		if (flippedCards1[0].querySelector('.back').style.backgroundImage === flippedCards1[1].querySelector('.back').style.backgroundImage){
+
+			//clear the flipped list
+			flippedCards1 = [];
+
+			score1 ++;
+			//console.log(score);
+
+			
+			scoreD1.querySelector('h2').innerText = score1;
+			if (score1 == 3){
+				gameOverDisplay();
+				
+			}
+		}
+
+		//if they fail to match
+		else{
+			//wait for 1 sec, flipBack the card.
+			setTimeout(turnBack1, 1000);
+		}
+	}
+
+
+
+	//------DONE------------------------------------------------------
+	function shuffle(arr){ 
+	//arr here will be the the collection of cards each player has
+		for (var i = 0; i < arr.length; i++){
+			//generate a random index
+			var u = Math.floor(Math.random() * arr.length);
+
+			//switch each card with a card of random index in the arr 
+			var tmp = arr[i];
+			arr[i] = arr[u];
+			arr[u] = tmp;
+		}
+		return arr;
+	}
+
+	//------------------------------------------------------------
+
+
+
+	function gameOverDisplay(){
+		var restart = document.getElementsByTagName('button')[0];
+    	restart.addEventListener('click', setBoard);
+
+    	endGame.style.display = 'flex';
+
+    	if (score > score1){
+    		endGame.querySelector('h1').innerText = 'Player1 Won';
+    	}
+    	else {
+    		endGame.querySelector('h1').innerText = 'Player2 Won';
+    	}
+	}
+
+	setBoard();
+	
+
+
+
+})();
+
+
